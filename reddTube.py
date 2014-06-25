38c09ef98a31457837c0b3a521809747a590519d
Xurxo Fresco <xurxof@gmail.com>
2014-06-25 19:24:19 +0200

    Change signature to getplaylist

--- reddTube.py
+++ reddTube.py
@@ -1,20 +1,43 @@
 import gdata.youtube.service
 import sys
 
+def get_first(iterable, default=None):
+    if iterable:
+        for item in iterable:
+            return item
+    return default
 
 
-
-
-def getplaylist (playlist_feed, playlist_name):
+def getplaylist (yt_service, playlist_name):
 	"""
-	playlist_feed:  YouTubePlaylistEntry
+	yt_service:  YouTube service
 	playlist: name to search
+	Retruns the playlist object or None
 	"""
+	playlist_feed = yt_service.GetYouTubePlaylistFeed(username='default')
 	entries = [entry for entry in playlist_feed.entry if entry.title.text == playlist_name]
-	return entries
+	return get_first(entries)
+
+def create_playlist (yt_service, playlist_name, playlist_description, playlist_public):
+	"""
+	yt_service:  YouTube service
+	playlist_name: name for the playlist
+	playlist_description: description for the playlist
+	playlist_public: true for create a public playlist
+	Returns the playlist
+	"""
+	new_playlistentry = yt_service.AddPlaylist(playlist_name, playlist_description, playlist_public)
+
+	if isinstance(new_playlistentry, gdata.youtube.YouTubePlaylistEntry):
+		return new_playlistentry
+	else:
+		return None
+
 
 
 playlist_name = sys.argv[1]
+playlist_description = playlist_name
+playlist_public = True
 
 # Create a client class which will make HTTP requests with Google Docs server.
 with open('password.hide') as f:
@@ -30,8 +53,9 @@ yt_service.client_id = 'reddTube'
 print yt_service.ProgrammaticLogin()
 
 # read user playlists
-playlist_feed = yt_service.GetYouTubePlaylistFeed(username='default')
 
-print getplaylist (playlist_feed, playlist_name)
 
+playlist= getplaylist (yt_service, playlist_name)
+if not playlist:
+	playlist = create_playlist (yt_service, playlist_name, playlist_description, playlist_public)
 
