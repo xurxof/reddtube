e9d4d7848795d751d3a88a0dc391d143fe4fa6a5
Xurxo Fresco <xurxof@gmail.com>
2014-07-30 20:52:24 +0200

    config file added. Better error support

--- reddTube.py
+++ reddTube.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import sys
 import time
 
@@ -18,14 +19,19 @@ def getplaylist(yt_service, playlist_name):
     """
     yt_service:  YouTube service
     playlist: name to search
-    Retruns the playlist object or None
+    Returns the playlist object or None
     """
     playlist_feed = yt_service.GetYouTubePlaylistFeed(username='default')
     entries = [entry for entry in playlist_feed.entry if entry.title.text == playlist_name]
     return get_first(entries)
 
 
-def create_playlist(yt_service, playlist_name, playlist_description, playlist_public):
+def get_playlist_description(original_title, original_url):
+    message = '{0}, an automatic playlist created with the links of {1}. Last update: {2}'
+    return message.format(original_title, original_url, datetime.today().strftime("%X %x"))
+
+
+def create_playlist(yt_service, playlist_name, original_url, private=True):
     """
     yt_service:  YouTube service
     playlist_name: name for the playlist
@@ -33,7 +39,8 @@ def create_playlist(yt_service, playlist_name, playlist_description, playlist_pu
     playlist_public: true for create a public playlist
     Returns the playlist
     """
-    new_playlistentry = yt_service.AddPlaylist(playlist_name, playlist_description, playlist_public)
+    playlist_description = get_playlist_description(playlist_name, original_url)
+    new_playlistentry = yt_service.AddPlaylist(playlist_name, playlist_description, private)
 
     if isinstance(new_playlistentry, gdata.youtube.YouTubePlaylistEntry):
         return new_playlistentry
@@ -41,7 +48,7 @@ def create_playlist(yt_service, playlist_name, playlist_description, playlist_pu
         return None
 
 
-def clear_playlist(yt_service, playlist, playlist_description):
+def clear_playlist(yt_service, playlist, original_url):
     """
     playlist:  YouTubePlaylistEntry object representing playlist to clear
     playlist_description: new description for the playlist
@@ -49,13 +56,13 @@ def clear_playlist(yt_service, playlist, playlist_description):
     """
     # playlist.id.text is something like http://gdata.youtube.com/feeds/api/users/bQQl1gQ/playlists/PLyhw-VhRP-i3AwJBx
     playlist_id = playlist.id.text.split('/')[-1]
-
     original_title = playlist.title.text
-    # updated_playlist = yt_service.UpdatePlaylist(playlist_id,
-    #	                                         original_title,
-    #	                                         playlist_description)
+    playlist_description = get_playlist_description(original_title, original_url)
+
+    yt_service.UpdatePlaylist(playlist_id, original_title, playlist_description)
 
     playlist_video_feed = yt_service.GetYouTubePlaylistVideoFeed(playlist_id=playlist_id)
+
     for video_entry in playlist_video_feed.entry:
         video_id = video_entry.id.text.split('/')[-1]
         yt_service.DeletePlaylistVideoEntry('http://gdata.youtube.com/feeds/api/playlists/' + playlist_id, video_id)
@@ -64,7 +71,7 @@ def clear_playlist(yt_service, playlist, playlist_description):
 def get_video_id_from_url(video_url):
     import urlparse
 
-    if 'www.youtube.com' in video_url:
+    if 'www.youtube.com' in video_url or 'feature=youtu.be' in video_url:
         parsed = urlparse.urlparse(video_url)
         video_v_parameters = urlparse.parse_qs(parsed.query)
         if 'v' not in video_v_parameters:
@@ -72,25 +79,33 @@ def get_video_id_from_url(video_url):
             video_id = None
         else:
             video_id = video_v_parameters['v'][0]
-    if 'youtu.be' in video_url:
+    elif 'youtu.be' in video_url:
         video_id = video_url.split('/')[-1]
     return video_id
 
 
 def add_video_playlist(yt_service, playlist, video_id):
-
     if not video_id:
         return
-    try:
-        playlist_id = playlist.id.text.split('/')[-1]
-        playlist_uri = 'http://gdata.youtube.com/feeds/api/playlists/' + playlist_id
-        yt_service.AddPlaylistVideoEntryToPlaylist(playlist_uri, video_id)
-    except gdata.service.RequestError as inst:
-        response = inst[0]
-        status = response['status']
-        reason = response['reason']
-        body = response['body']
-        print "Request error. Status: {1} Reason:{2} Body: {3}".format(response, status, reason, body)
+    max_attempts=5
+    attempts = 0
+
+    while attempts < max_attempts:
+        try:
+            playlist_id = playlist.id.text.split('/')[-1]
+            playlist_uri = 'http://gdata.youtube.com/feeds/api/playlists/' + playlist_id
+            yt_service.AddPlaylistVideoEntryToPlaylist(playlist_uri, video_id)
+            break
+        except gdata.service.RequestError as inst:
+            response = inst[0]
+            status = response['status']
+            reason = response['reason']
+            body = response['body']
+            print "Request error. Attemp: {4} Status: {1} Reason:{2} Body: {3}".format(response, status, reason, body, attempts)
+            attempts += 1
+            if status in [403, 400, 404]:  # 403 - forbidden; 400 - bad request body; 404 - not found;
+                break
+            time.sleep(5)
 
 
 def get_all_youtube_url(url_origin):
@@ -98,42 +113,38 @@ def get_all_youtube_url(url_origin):
     # get youtube links from origin url
     from BeautifulSoup import BeautifulSoup
     import urllib2
-
+    max_attempts = 5
     attempts = 0
-    done = False
-    while attempts < 5 and not done:
+
+    while attempts < max_attempts:
         try:
             html_page = urllib2.urlopen(url_origin)
+            break
         except urllib2.HTTPError as inst:
-            print "HTTPError. Attemps: {0} Error: {1}".format(attempts, inst)
+            print "HTTPError. Attempts: {0} Error: {1}".format(attempts, inst)
             attempts += 1
-            time.sleep(10)
-        else:
-            print "Analyzing {0}".format(url_origin)
-            soup = BeautifulSoup(html_page)
-            for soup_link in soup.findAll('a'):
-                link = soup_link.get('href')
-                if link is None:
-                    continue
-                if 'domain/youtu.be' in link:
-                    continue
-                if ('www.youtube.com' in link or 'youtu.be' in link):
-                    # TODO: best check
-                    links.append(link)
-            done = True
-    video_ids=list(set([get_video_id_from_url(url) for url in links]))
+            time.sleep(4)
+    if not html_page:
+        return []
+
+    print "Analyzing {0}".format(url_origin)
+    soup = BeautifulSoup(html_page)
+    for soup_link in soup.findAll('a'):
+        link = soup_link.get('href')
+        if link is None:
+            continue
+        if 'domain/youtu.be' in link:
+            continue
+        if 'www.youtube.com' in link or 'youtu.be' in link:
+            # TODO: best check
+            links.append(link)
+
+    video_ids = list(set([get_video_id_from_url(url) for url in links]))
     return video_ids
 
+    # Create a client class which will make HTTP requests with Google Docs server.
 
 
-playlist_name = sys.argv[1]
-playlist_description = playlist_name
-playlist_public = True
-url_origin = sys.argv[2]
-
-print 'Updating ' + playlist_name + ' with ' + url_origin
-
-# Create a client class which will make HTTP requests with Google Docs server.
 with open('password.hide') as f:
     username, password = f.readline().strip().split(':')
     developer_key = f.readline().strip()
@@ -147,23 +158,27 @@ yt_service.client_id = 'reddTube'
 yt_service.ProgrammaticLogin()
 print 'Logged'
 
+cfg_playlists = [map(str.strip,line.split('|')) for line in open('playlists.cfg')]
 
-video_ids = get_all_youtube_url(url_origin)
-if not video_ids:
-    sys.exit('No video links founded. Playlist not updated')
 
-# unique video ids
+for cfg_playlist in cfg_playlists:
+    playlist_name, url_origin = cfg_playlist[0:2]
+    print 'Updating {0} with {1}'.format(playlist_name, url_origin)
 
-# read user playlists
-playlist = getplaylist(yt_service, playlist_name)
-if not playlist:
-    playlist = create_playlist(yt_service, playlist_name, playlist_description, playlist_public)
-else:
-    clear_playlist(yt_service, playlist, playlist_description)
+    # unique video ids
+    video_ids = get_all_youtube_url(url_origin)
+    if not video_ids:
+        sys.exit('No video links founded. Playlist not updated')
 
-for video_id in video_ids:
-    time.sleep(3)  # flood control
-    print 'adding ' + video_id
-    add_video_playlist(yt_service, playlist, video_id)
+    # read user play lists
+    playlist = getplaylist(yt_service, playlist_name)
+    if not playlist:
+        playlist = create_playlist(yt_service, playlist_name, url_origin)
+    else:
+        clear_playlist(yt_service, playlist, url_origin)
+
+    for video_id in video_ids:
+        print 'adding ' + video_id
+        add_video_playlist(yt_service, playlist, video_id)
 
 
